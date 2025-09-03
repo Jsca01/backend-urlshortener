dd37984c523fabe5cbfba692c082a4e286501311
jesus <jesu.reinor@zoho.com>
2025-09-03 03:31:34 -0400

    curso

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const dns = require("dns");
 const app = express();
 let bodyParser = require('body-parser');
 
@@ -26,7 +25,6 @@ app.get('/api/hello', function(req, res) {
 app.post("/api/shorturl", (req,res) =>{
 	let url = req.body.url.toString();
 	if (!(url.includes("https://") || url.includes("http://"))){
-		console.log("no entre :c")
 		return res.json({"error":'invalid url'});
 	}
 	let id = myUrls.length;
@@ -34,12 +32,11 @@ app.post("/api/shorturl", (req,res) =>{
 	return res.json({"original_url":url,"short_url":id});
 });
 app.get("/api/shorturl/:id", (req,res) =>{
-	let id = req.params.id
-	if(!myUrls.lenfth < id){return res.send("invalid");}
+	let id = req.params.id;
+	if(myUrls.length < id){return res.send("invalid");}
 	let url = myUrls[id].value;
-	console.log(url);
-	return res.redirect(myUrls[id].value);
-});
+	return res.redirect(303, myUrls[id].value);
+	});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
