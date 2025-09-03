b38e10c0244c9f92c0d5bbcb4d63d6463289529b
jesus <jesu.reinor@zoho.com>
2025-09-03 02:48:56 -0400

    curso

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require("dns");
 const app = express();
+let bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
+
+var myUrls = []; // mongo not required for use var 
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +21,24 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post("/api/shorturl", (req,res) =>{
+	let url = req.body.url.toString();
+	if (!(url.includes("https://") || url.includes("http://"))){
+		console.log("no entre :c")
+		return res.json({"error":'invalid url'});
+	}
+	let id = myUrls.length;
+	myUrls.push({"value":url});
+	return res.json({"original_url":url,"short_url":id});
+});
+app.get("/api/shorturl/:id", (req,res) =>{
+	let id = req.params.id
+	if(!myUrls.lenfth < id){return res.send("invalid");}
+	let url = myUrls[id].value;
+	console.log(url);
+	return res.redirect(myUrls[id].value);
 });
 
 app.listen(port, function() {
